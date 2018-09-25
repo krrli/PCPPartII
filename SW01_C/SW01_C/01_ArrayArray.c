e676bfe9acaa0175ccc40199844502fe043115ea
Carla Iten <carlaiten@gmail.com>
2018-09-25 17:34:10 +0200

    Added gitignore for VS

---
+++ SW01_C/SW01_C/01_ArrayArray.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+// typischer imperativen C-Code mit einer goto - Anweisung
+// 0 1 2 3 4 5 6 7 = printNumbersGoto(7)
+void printNumbersGoto(int untilNumberX) {		
+
+	if (untilNumberX < 0) return;	
+
+	int i = 0;
+	iPlusPlusAndContinue:
+	printf("%i ", i);
+	i++;
+	if (i <= untilNumberX) goto iPlusPlusAndContinue;
+
+	printf("= printNumbersGoto(%i)\n ", untilNumberX);
+
+}
+
+// typischer strukturierten C-Code mit einer for-Anweisung
+void printNumbersFor(int untilNumberX) {
+
+}
+
+// typischer prozeduraler Code mit einer rekursiven Funktion
+void printNumbersRecursiveFunction() {
+
+}
+
+int main() {
+	printNumbersGoto(7);
+
+	return 0;
+}
+
