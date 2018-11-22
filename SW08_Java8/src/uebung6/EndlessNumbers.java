3b67b9b446776bfe107abb2e91df8c3a7d628492
Carla Iten <carlaiten@gmail.com>
2018-11-22 11:44:01 +0100

    finished exercise week 08

---
+++ SW08_Java8/src/uebung6/EndlessNumbers.java
@@ -0,0 +1,37 @@
+package uebung6;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class EndlessNumbers {
+
+    public static void main(String[] args) {
+
+        // a)
+        Random random = new Random();
+        boolean foundAny = random.ints().anyMatch(i -> ((i >= 10_000) && (i <= 12_000)));
+        System.out.println("foundAny = " + foundAny);
+
+        // b)
+        AtomicInteger atomicInteger = new AtomicInteger();
+        random.ints().anyMatch(i -> {
+            atomicInteger.incrementAndGet();
+            return ((i >= 10_000) && (i <= 12_000));
+        });
+        System.out.println("Found suitable number at stream position " + atomicInteger.get());
+    }
+
+}
+
+/**
+ * 6a)
+ * random: ints() von Random
+ * anyMatch() Stream Operation
+ * anyMatch ist short circuiting Operation (abbrechen, bevor letztes Element erreicht ist)
+ * und  terminal -> Stream ist nach Konsumierung aufgebraucht!
+ *
+ * 6b)
+ * Andere Lösung?
+ *
+ */
