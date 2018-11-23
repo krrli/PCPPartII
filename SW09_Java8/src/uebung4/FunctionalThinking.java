276e8eabd2121c5fe406069d11dac71032c404c1
Carla Iten <carlaiten@gmail.com>
2018-11-23 10:35:14 +0100

    finished exercise week 09

---
+++ SW09_Java8/src/uebung4/FunctionalThinking.java
@@ -0,0 +1,40 @@
+package uebung4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FunctionalThinking {
+
+    public static void main(String[] args){
+        List<String> strings = Arrays.asList("Java", "is", "cool");
+        FunctionalThinking ft = new FunctionalThinking();
+
+        String java7joinedString = ft.join(strings);
+        System.out.println("------ 4a)------ ");
+        System.out.println(java7joinedString);
+        String message = String.join(" ", strings);
+
+
+    }
+
+    /*
+        4a): S. Implementierung
+        4b): Verwendung von Kontrollstrukturen zur Steuerung der Befehlsausführung - z.B. loop (foreach)
+        4c): Programme sind kurz, klar, gut zu warten, zuverlässig und schnell zu erstellen
+             Verkettung von Funktionsaufrufen
+
+             Programme bestehen aus Definitionen von Funktionen mit Parametern und Rückgabewerten
+             Rückgabewert hängt ausschliesslich von den Parametern ab
+             Aufgaben in kleinere zerlegen, und sie wieder zusammenführen (dekomposition und komposition)
+        4d): Fokussierung auf Problem - was will man lösen? Weiter weg von Details (z.B. Loop)
+     */
+    public String join(List<String> strings){
+        String joinedString = "";
+        for(String string : strings){
+            joinedString += string + " ";
+        }
+
+        return joinedString;
+    }
+}
